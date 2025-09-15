281ce2d6b503ae4475ffc8baf96dbb428ff3e8dc
Eline Bisschop <ebisschop@tudelft.nl>
2025-09-15 21:10:42 +0200

    new version signals

--- signals.py
+++ signals.py
@@ -2,4 +2,7 @@ import numpy as np
 
 def generate_sine_wave(frequency, duration, sample_rate):
     t=np.linspace(0, duration, int(sample_rate*duration))
-    return np.sin(2*np.pi*frequency*t)
+    y= np.sin(2*np.pi*frequency*t)
+    return t, y
+
+

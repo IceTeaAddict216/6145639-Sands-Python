dc4e6bbcb0c560a7de7955f1778df726b76368b6
Eline Bisschop <ebisschop@tudelft.nl>
2025-09-15 21:17:07 +0200

    Run function sine wave with plot

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
-from signals import generate_sine_wave
-print(generate_sine_wave(5, 2, 100)[0:10])
+import signals as s
+print(s.generate_sine_wave(5, 2, 100)[0:10])
+
+import matplotlib.pyplot as plt
+
+t,y=s.generate_sine_wave(5,2,100)
+
+plt.plot(t, y)
+plt.title("Generated sine wave")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
+plt.show()

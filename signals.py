5a8d30e251ee7045dc7458c85aaa1c48c2a7f5d4
Eline Bisschop <ebisschop@tudelft.nl>
2025-10-02 22:26:48 +0200

    defined two unit step functions + unit pulse function

--- signals.py
+++ signals.py
@@ -5,4 +5,11 @@ def generate_sine_wave(frequency, duration, sample_rate):
     y= np.sin(2*np.pi*frequency*t)
     return t, y
 
+def unit_step(n):
+    return np.heaviside(n,1)
 
+def unit_step2(n2):
+    return np.where(n2 < 0, 0, 2)
+
+def unit_pulse(x):
+    return np.where(np.abs(x) <= 0.5, 5, 0)
